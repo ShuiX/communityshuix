35cbd18c87fea7c171bbc2c8f1a4971c2e2669c9
ShuiX <shajith.307@gmail.com>
2020-09-22 14:10:43 +0200

    only followers show up

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from './user.model';
-import { Post } from './post.model';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, DocumentChangeAction } from '@angular/fire/firestore';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogsComponent } from '../dialogs/dialogs.component';
@@ -20,6 +19,8 @@ export class AuthService {
     private router: Router,
     private dialog: MatDialog
   ) { }
+
+  userFollowers = new BehaviorSubject(null);
 
   signInWithEmail(email: string, password: string): Promise<firebase.auth.UserCredential> {
     return this.afAuth.signInWithEmailAndPassword(email, password);
@@ -42,6 +43,7 @@ export class AuthService {
     return this.afAuth.authState.pipe(
       switchMap(user => {
         if (user) {
+          this.afs.doc<any>(`users/${user.uid}`).valueChanges().subscribe(val => this.userFollowers.next(val.following));
           return this.afs.doc<any>(`users/${user.uid}`).valueChanges();
         } else {
           return of(null);
@@ -51,7 +53,7 @@ export class AuthService {
   }
 
   getPosts(lastseen: string): Observable<DocumentChangeAction<unknown>[]> {
-    return this.afs.collection("posts", ref => ref.orderBy("title").startAfter(lastseen).limit(10)).snapshotChanges();
+    return this.afs.collection("posts", ref => ref.where("createdByUID", "in", this.userFollowers.value).orderBy("postDate").startAfter(lastseen).limit(10)).snapshotChanges();
   }
 
   signOut(): void {
